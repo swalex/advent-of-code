8bb180492562372a9fc7e93e3be3044c45e70309
Alexander Saratow <a@saratow.biz>
2022-12-19 14:13:42 +0100

    solved day 6 quests

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -6,21 +6,52 @@ internal static class Program
     {
         string sequence = File.ReadAllText("input.txt");
 
-        int duplicateAt = 0;
-        int position1 = 0;
+        int position1 = new Scanner(4).Scan(sequence);
+        int position2 = new Scanner(14).Scan(sequence);
 
-        var header = new Queue<char>();
+        Console.WriteLine($"1/2 First Marker after {position1} characters.");
+        Console.WriteLine($"2/2 First Message after {position2} characters.");
+    }
 
-        for (var i = 0; i < 3; i++)
-        {
-            
-        }
+    private sealed class Scanner
+    {
+        private int _length;
         
-        for (var i = 0; i < sequence.Length; i++)
+        private readonly Queue<char> _values;
+
+        private int _duplicates;
+
+        internal Scanner(int headerLength)
         {
-            
+            _length = headerLength;
+            _values = new Queue<char>(headerLength);
         }
+
+        private bool FoundMarker =>
+            _values.Count == _length && _duplicates == 0;
+
+        internal int Scan(IEnumerable<char> data) =>
+            data.TakeWhile(Push).Count() + 1;
         
-        Console.WriteLine($"1/2 First Marker at {position1}");
+        private bool Push(char value)
+        {
+            PopOldValue();
+            PushNewValue(value);
+            return !FoundMarker;
+        }
+
+        private void PopOldValue()
+        {
+            if (_values.Count < _length) return;
+
+            char oldValue = _values.Dequeue();
+            if (_values.Contains(oldValue)) _duplicates--;
+        }
+
+        private void PushNewValue(char value)
+        {
+            if (_values.Contains(value)) _duplicates++;
+            _values.Enqueue(value);
+        }
     }
 }
