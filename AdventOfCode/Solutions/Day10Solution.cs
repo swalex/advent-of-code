00ee5bc0d653d0379cf61d8521bf7789254abee9
Alexander Saratow <a@saratow.biz>
2022-12-20 01:27:56 +0100

    solved second day 10 puzzle

--- AdventOfCode/Solutions/Day10Solution.cs
+++ AdventOfCode/Solutions/Day10Solution.cs
@@ -2,6 +2,8 @@ namespace AdventOfCode.Solutions;
 
 internal sealed class Day10Solution : SolutionBase
 {
+    private readonly char[] _buffer = new char[40 * 6];
+
     private int _cumulatedStrengths;
 
     private int _cycle;
@@ -19,18 +21,24 @@ internal sealed class Day10Solution : SolutionBase
             if (IsNoop(line)) Tick();
             else ProcessAdd(line.Replace("addx ", string.Empty));
         }
-        
-        return BuildResult(_cumulatedStrengths, 0);
+
+        return BuildResult(_cumulatedStrengths, Dump(_buffer));
     }
 
-    private static SolutionResult BuildResult(int sumOfStrengths, int bar) =>
-        ($"Sum of signal strengths: {sumOfStrengths}", "Bar");
+    private static SolutionResult BuildResult(int sumOfStrengths, string dump) =>
+        ($"Sum of signal strengths: {sumOfStrengths}", "Screen Snapshot:\n" + dump);
+
+    private static string Dump(IEnumerable<char> buffer) =>
+        string.Join('\n', buffer.Chunk(40).Select(c => new string(c.ToArray())));
 
     private static bool IsNoop(string line) =>
         line == "noop";
 
     private static bool IsSignificant(int cycle) =>
         (cycle + 20) % 40 == 0;
+
+    private static bool IsVisible(int cycle, int position) =>
+        cycle >= position - 1 && cycle <= position + 1;
 
     private void Cumulate(int value) =>
         _cumulatedStrengths += value;
@@ -47,6 +55,8 @@ internal sealed class Day10Solution : SolutionBase
 
     private void Tick()
     {
+        _buffer[_cycle % (40 * 6)] = IsVisible(_cycle % 40, _x) ? '#' : '.';
+
         _cycle++;
         if (IsSignificant(_cycle)) Cumulate(_cycle * _x);
     }
