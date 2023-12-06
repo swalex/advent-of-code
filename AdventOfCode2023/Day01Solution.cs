144906e40d625fb7988612352f24218e04a07337
Alexander Saratow <a@saratow.biz>
2023-12-06 22:42:00 +0100

    solve first puzzle for day 1; fail on the second one

---
+++ AdventOfCode2023/Day01Solution.cs
@@ -0,0 +1,35 @@
+namespace AdventOfCode2023;
+
+internal static class Day01Solution
+{
+    private static readonly List<string> Numbers = new()
+    {
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+    };
+
+    internal static int SolveFirstPuzzle(IEnumerable<string> input) =>
+        input.Select(BuildNumber).Sum();
+
+    internal static int SolveSecondPuzzle(IEnumerable<string> input)
+    {
+        return SolveFirstPuzzle(input.Select(ConvertDigits));
+    }
+
+    private static string ConvertDigits(string line) =>
+        Numbers.Aggregate(line, ReplaceTextDigit);
+
+    private static string ReplaceTextDigit(string current, string number) =>
+        current.Replace(number, AsDigit(number));
+
+    private static string AsDigit(string number) =>
+        AsIntegerDigit(number).ToString();
+
+    private static int AsIntegerDigit(string number) =>
+        Numbers.IndexOf(number) + 1;
+
+    private static int BuildNumber(string line)
+    {
+        List<int> digits = line.Where(char.IsDigit).Select(c => c - '0').ToList();
+        return digits.First() * 10 + digits.Last();
+    }
+}
---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode2023;
+
+Console.WriteLine("Advent of Code 2023");
+
+string[] input = File.ReadAllLines("InputData/day01.txt");
+Console.WriteLine($"Day  1 - Puzzle 1: {Day01Solution.SolveFirstPuzzle(input)}");
+Console.WriteLine($"Day  1 - Puzzle 2: {Day01Solution.SolveSecondPuzzle(input)}");
