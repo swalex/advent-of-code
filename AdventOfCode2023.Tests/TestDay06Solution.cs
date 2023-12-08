b8ad5924ef66128e8f2285b08ec1b48e5e8db0b0
Alexander Saratow <a@saratow.biz>
2023-12-08 19:41:57 +0100

    implement first day 6 solution

--- AdventOfCode2023.Tests/TestDay06Solution.cs
+++ AdventOfCode2023.Tests/TestDay06Solution.cs
@@ -2,5 +2,36 @@ namespace AdventOfCode2023.Tests;
 
 public sealed class TestDay06Solution
 {
+    private const string ExampleData =
+        """
+        Time:      7  15   30
+        Distance:  9  40  200
+        """;
 
+    private static readonly int[] ExpectedOptions = { 4, 8, 9 };
+
+    private static int FirstExpectedResult =>
+        ExpectedOptions.Aggregate(1, (a, b) => a * b);
+
+    public static IEnumerable<object[]> EnumerateExpectedOptions() =>
+        ExpectedOptions.Select((option, index) => new object[] { index, option });
+
+    [Theory]
+    [MemberData(nameof(EnumerateExpectedOptions))]
+    public void VerifyExpectedOption(int index, int option)
+    {
+        List<int> options = Day06Solution.EnumerateOptions(ExampleData.Lines()).ToList();
+
+        Assert.Equal(option, options[index]);
+    }
+
+    [Fact]
+    public void VerifyFirstSolution()
+    {
+        int expected = FirstExpectedResult;
+
+        long actual = new Day06Solution().SolveFirstPuzzle(ExampleData.Lines());
+
+        Assert.Equal(expected, actual);
+    }
 }
--- AdventOfCode2023/Day06Solution.cs
+++ AdventOfCode2023/Day06Solution.cs
@@ -6,8 +6,40 @@ public sealed class Day06Solution : ISolution
         6;
 
     public long SolveFirstPuzzle(IReadOnlyList<string> input) =>
-        throw new NotImplementedException();
+        EnumerateOptions(input).Aggregate(1, (a, b) => a * b);
 
     public long SolveSecondPuzzle(IReadOnlyList<string> input) =>
         throw new NotImplementedException();
+
+    public static IEnumerable<int> EnumerateOptions(IEnumerable<string> lines) =>
+        ParseSetup(lines).EnumerateOptions().Select(o => o.Count());
+
+    private static Setup ParseSetup(IEnumerable<string> lines) =>
+        BuildSetup(SplitInput(lines).ToList());
+
+    private static Setup BuildSetup(IReadOnlyList<IEnumerable<int>> splitInput) =>
+        new(splitInput[0].ToArray(), splitInput[1].ToArray());
+
+    private static IEnumerable<IEnumerable<int>> SplitInput(IEnumerable<string> lines) =>
+        lines.Select(SplitLine);
+
+    private static IEnumerable<int> SplitLine(string line) =>
+        line.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+            .Skip(1)
+            .Select(int.Parse);
+
+    internal sealed record Setup(int[] Times, int[] Distances)
+    {
+        internal IEnumerable<IEnumerable<int>> EnumerateOptions() =>
+            Times.Select((time, index) =>
+                EnumerateOptionsFromTime(time).Where(distance => distance > Distances[index]));
+
+        private static IEnumerable<int> EnumerateOptionsFromTime(int time) =>
+            Enumerable
+                .Range(0, time)
+                .Select(ComputeDistance(time));
+
+        private static Func<int, int> ComputeDistance(int time) =>
+            acceleration => (time - acceleration) * acceleration;
+    }
 }
