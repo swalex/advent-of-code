ad3fd29bf76e9c93a95801745989510dfae669ff
Alexander Saratow <a@saratow.biz>
2023-12-08 21:26:02 +0100

    implement second day 7 solution

--- AdventOfCode2023.Tests/TestDay07Solution.cs
+++ AdventOfCode2023.Tests/TestDay07Solution.cs
@@ -11,33 +11,56 @@ public sealed class TestDay07Solution
         QQQJA 483
         """;
 
-    private static readonly int[] Ranks = { 1, 4, 3, 2, 5 };
+    private static readonly int[] FirstPuzzleRanks = { 1, 4, 3, 2, 5 };
+
+    private static readonly int[] SecondPuzzleRanks = { 1, 3, 2, 5, 4 };
 
     private static readonly int[] Bids = { 765, 684, 28, 220, 483 };
 
-    private static IEnumerable<int> OrderedBids =>
-        Ranks.Zip(Bids).OrderBy(zip => zip.First).Select(zip => zip.Second);
+    private static IEnumerable<int> Order(IEnumerable<int> ranks) =>
+        ranks.Zip(Bids).OrderBy(zip => zip.First).Select(zip => zip.Second);
 
-    private static IEnumerable<int> Winnings =>
-        Ranks.Zip(Bids, (r, b) => r * b);
+    private static IEnumerable<int> Winnings(IEnumerable<int> ranks) =>
+        ranks.Zip(Bids, (r, b) => r * b);
 
-    private static int ExpectedWinnings =>
-        Winnings.Sum();
+    private static int ExpectedFirstWinnings =>
+        Winnings(FirstPuzzleRanks).Sum();
+
+    private static int ExpectedSecondWinnings =>
+        Winnings(SecondPuzzleRanks).Sum();
 
     [Fact]
-    public void VerifyHandOrder()
+    public void VerifyFirstHandOrder()
     {
         IEnumerable<Day07Solution.Hand> hands = Day07Solution.EnumerateOrderedHands(ExampleData.Lines());
 
-        Assert.Equal(OrderedBids, hands.Select(h => h.Bid));
+        Assert.Equal(Order(FirstPuzzleRanks), hands.Select(h => h.Bid));
     }
 
     [Fact]
     public void VerifyFirstSolution()
     {
-        int expected = ExpectedWinnings;
+        int expected = ExpectedFirstWinnings;
 
         long actual = new Day07Solution().SolveFirstPuzzle(ExampleData.Lines());
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void VerifySecondHandOrder()
+    {
+        IEnumerable<Day07Solution.Hand> hands = Day07Solution.EnumerateOrderedHandsWithJoker(ExampleData.Lines());
+
+        Assert.Equal(Order(SecondPuzzleRanks), hands.Select(h => h.Bid));
+    }
+
+    [Fact]
+    public void VerifySecondSolution()
+    {
+        long expected = ExpectedSecondWinnings;
+
+        long actual = new Day07Solution().SolveSecondPuzzle(ExampleData.Lines());
 
         Assert.Equal(expected, actual);
     }
--- AdventOfCode2023/Day07Solution.cs
+++ AdventOfCode2023/Day07Solution.cs
@@ -2,6 +2,10 @@ namespace AdventOfCode2023;
 
 public sealed class Day07Solution : ISolution
 {
+    private const string FirstDeck = "AKQJT98765432";
+
+    private const string SecondDeck = "AKQT98765432J";
+
     public int Day =>
         7;
 
@@ -11,21 +15,39 @@ public sealed class Day07Solution : ISolution
             .Sum();
 
     public long SolveSecondPuzzle(IReadOnlyList<string> input) =>
-        throw new NotImplementedException();
+        EnumerateOrderedHandsWithJoker(input)
+            .Select((hand, index) => hand.Bid * (index + 1))
+            .Sum();
 
     internal static IEnumerable<Hand> EnumerateOrderedHands(IEnumerable<string> lines) =>
         lines
             .Select(ParseHand)
             .GroupBy(HandType)
             .OrderBy(g => g.Key)
-            .SelectMany(g => g.OrderDescending(BySecond.Ordering));
+            .SelectMany(g => g.OrderDescending(BySecondOrdering.WithoutJoker));
 
-    private sealed class BySecond : IComparer<Hand>
+    internal static IEnumerable<Hand> EnumerateOrderedHandsWithJoker(IEnumerable<string> lines) =>
+        lines
+            .Select(ParseHand)
+            .GroupBy(HandTypeWithJoker)
+            .OrderBy(g => g.Key)
+            .SelectMany(g => g.OrderDescending(BySecondOrdering.WithJoker));
+
+    private sealed class BySecondOrdering : IComparer<Hand>
     {
-        internal static IComparer<Hand> Ordering { get; } = new BySecond();
+        private readonly string _deck;
+
+        private BySecondOrdering(string deck)
+        {
+            _deck = deck;
+        }
+
+        internal static IComparer<Hand> WithoutJoker { get; } = new BySecondOrdering(FirstDeck);
+
+        internal static IComparer<Hand> WithJoker { get; } = new BySecondOrdering(SecondDeck);
 
         public int Compare(Hand x, Hand y) =>
-            x.EnumerateHandValues().Zip(y.EnumerateHandValues(), (a, b) => a.CompareTo(b))
+            x.EnumerateHandValues(_deck).Zip(y.EnumerateHandValues(_deck), (a, b) => a.CompareTo(b))
                 .FirstOrDefault(c => c != 0);
     }
 
@@ -38,16 +60,26 @@ public sealed class Day07Solution : ISolution
     private static Hand.Type HandType(Hand hand) =>
         hand.GetHandType();
 
+    private static Hand.Type HandTypeWithJoker(Hand hand) =>
+        hand.GetHandTypeWithJoker();
+
     internal readonly record struct Hand(string Cards, int Bid)
     {
-        private const string Deck = "AKQJT98765432";
+        internal IEnumerable<int> EnumerateHandValues(string deck) =>
+            Cards.Select(card => deck.IndexOf(card));
 
-        internal IEnumerable<int> EnumerateHandValues() =>
-            Cards.Select(card => Deck.IndexOf(card));
+        internal Type GetHandTypeWithJoker() =>
+            GetHandType(Cards.Replace('J', Pretend(Cards)));
 
-        internal Type GetHandType()
+        private static char Pretend(IEnumerable<char> cards) =>
+            cards.Where(c => c != 'J').GroupBy(c => c).OrderBy(g => g.Count()).Last().Key;
+
+        internal Type GetHandType() =>
+            GetHandType(Cards);
+
+        private static Type GetHandType(IEnumerable<char> cards)
         {
-            List<IGrouping<char, char>> groups = Cards.GroupBy(c => c).ToList();
+            List<IGrouping<char, char>> groups = cards.GroupBy(c => c).ToList();
             return groups.Count switch
             {
                 5 => Type.HighCard,
