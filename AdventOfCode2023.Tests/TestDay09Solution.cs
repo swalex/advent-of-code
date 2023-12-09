1bd5725c275f6d3c09b039dc754b53269fc67b7a
Alexander Saratow <a@saratow.biz>
2023-12-09 12:53:46 +0100

    implement second day 9 solution

--- AdventOfCode2023.Tests/TestDay09Solution.cs
+++ AdventOfCode2023.Tests/TestDay09Solution.cs
@@ -11,17 +11,25 @@ public sealed class TestDay09Solution
 
     private static readonly int[] ExpectedFirstSubResults = { 18, 28, 68 };
 
+    private static readonly int[] ExpectedSecondSubResults = { -3, 0, 5 };
+
     private static int ExpectedFirstResult =>
         ExpectedFirstSubResults.Sum();
 
-    public static IEnumerable<object[]> EnumerateSingleExtrapolationData() =>
+    private static int ExpectedSecondResult =>
+        ExpectedSecondSubResults.Sum();
+
+    public static IEnumerable<object[]> EnumerateFirstSingleExtrapolationData() =>
         ExampleData.Lines().Zip(ExpectedFirstSubResults, (line, expectedSum) => new object[] { line, expectedSum });
 
+    public static IEnumerable<object[]> EnumerateSecondSingleExtrapolationData() =>
+        ExampleData.Lines().Zip(ExpectedSecondSubResults, (line, expectedSum) => new object[] { line, expectedSum });
+
     [Theory]
-    [MemberData(nameof(EnumerateSingleExtrapolationData))]
-    public void VerifySingleExtrapolation(string line, int expectedSum)
+    [MemberData(nameof(EnumerateFirstSingleExtrapolationData))]
+    public void VerifySingleForwardExtrapolation(string line, int expectedSum)
     {
-        long actual = Day09Solution.Extrapolate(line);
+        long actual = Day09Solution.ExtrapolateForward(line);
 
         Assert.Equal(expectedSum, actual);
     }
@@ -32,5 +40,22 @@ public sealed class TestDay09Solution
         long actual = new Day09Solution().SolveFirstPuzzle(ExampleData.Lines());
 
         Assert.Equal(ExpectedFirstResult, actual);
+    }
+
+    [Theory]
+    [MemberData(nameof(EnumerateSecondSingleExtrapolationData))]
+    public void VerifySingleBackwardExtrapolation(string line, int expectedSum)
+    {
+        long actual = Day09Solution.ExtrapolateBackward(line);
+
+        Assert.Equal(expectedSum, actual);
+    }
+
+    [Fact]
+    public void VerifySecondSolution()
+    {
+        long actual = new Day09Solution().SolveSecondPuzzle(ExampleData.Lines());
+
+        Assert.Equal(ExpectedSecondResult, actual);
     }
 }
--- AdventOfCode2023/Day09Solution.cs
+++ AdventOfCode2023/Day09Solution.cs
@@ -6,25 +6,30 @@ public sealed class Day09Solution : ISolution
         9;
 
     public long SolveFirstPuzzle(IReadOnlyList<string> input) =>
-        input.Select(Extrapolate).Sum();
+        input.Select(ExtrapolateForward).Sum();
 
     public long SolveSecondPuzzle(IReadOnlyList<string> input) =>
-        throw new NotImplementedException();
+        input.Select(ExtrapolateBackward).Sum();
 
-    internal static long Extrapolate(string line) =>
-        Extrapolate(ParseLine(line));
+    internal static long ExtrapolateForward(string line) =>
+        Extrapolate(ParseLine(line), values => values.Last(), +1);
 
-    private static long Extrapolate(IReadOnlyCollection<long> values) =>
-        values.Last() + ComputeNextStep(values);
+    internal static long ExtrapolateBackward(string line) =>
+        Extrapolate(ParseLine(line), values => values.First(), -1);
 
-    private static long ComputeNextStep(IReadOnlyCollection<long> values)
+    private static long Extrapolate(IReadOnlyCollection<long> values,
+        Func<IReadOnlyCollection<long>, long> endSelector, long factor) =>
+        endSelector.Invoke(values) + ComputeStep(values, endSelector, factor);
+
+    private static long ComputeStep(IReadOnlyCollection<long> values, Func<IReadOnlyCollection<long>, long> endSelector, long factor)
     {
         List<long> differences = values.SkipLast(1).Zip(values.Skip(1), (a, b) => b - a).ToList();
 
-        long lastDifference = differences.Last();
-        return differences.GroupBy(d => d).Count() == 1
-            ? lastDifference
-            : lastDifference + ComputeNextStep(differences);
+        long endDifference = endSelector.Invoke(differences);
+        long result = differences.GroupBy(d => d).Count() == 1
+            ? endDifference
+            : endDifference + ComputeStep(differences, endSelector, factor);
+        return result * factor;
     }
 
     private static long[] ParseLine(string line) =>
