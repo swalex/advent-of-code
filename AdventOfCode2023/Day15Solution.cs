2c7f69facf3119753cb3df9399ff16c3164cf1a9
Alexander Saratow <a@saratow.biz>
2023-12-15 15:29:16 +0100

    implement second day 15 solution

--- AdventOfCode2023/Day15Solution.cs
+++ AdventOfCode2023/Day15Solution.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace AdventOfCode2023;
 
 public sealed class Day15Solution : ISolution
@@ -9,7 +11,28 @@ public sealed class Day15Solution : ISolution
         input.Select(SolveFirstPuzzleOnLine).Sum();
 
     public long SolveSecondPuzzle(IReadOnlyList<string> input) =>
-        throw new NotImplementedException();
+        input.Aggregate(0L, (acc, l) => acc + SolveSecondPuzzle(l));
+
+    private static long SolveSecondPuzzle(string input)
+    {
+        Box[] boxes = Enumerable.Repeat(0, 256).Select(_ => new Box()).ToArray();
+
+        foreach (string task in input.Split(','))
+        {
+            var lens = Lens.DecodeLens(task);
+            if (lens.FocalLength < 0)
+            {
+                boxes[lens.BoxIndex].Remove(lens);
+            }
+            else
+            {
+                boxes[lens.BoxIndex].AddOrReplace(lens);
+            }
+        }
+
+        return boxes.Select((b, bi) => (bi + 1) * b.Select((b, i) => b.FocalLength * (i + 1))
+            .Aggregate(0, (i, l) => i + l)).Sum();
+    }
 
     private static long SolveFirstPuzzleOnLine(string line) =>
         line.Split(',').Select(ComputeHash).Sum();
@@ -26,26 +49,53 @@ public sealed class Day15Solution : ISolution
             (value + letter) * 17 % 256;
     }
 
-    internal sealed class Lens
+    internal sealed class Box : IEnumerable<Lens>
+    {
+        private readonly LinkedList<Lens> _lenses = new();
+
+        internal void AddOrReplace(Lens lens)
+        {
+            LinkedListNode<Lens>? node = _lenses.Find(lens);
+            if (node != null) node.Value = lens;
+            else _lenses.AddLast(lens);
+        }
+
+        internal void Remove(Lens lens) =>
+            _lenses.Remove(lens);
+
+        public IEnumerator<Lens> GetEnumerator() =>
+            _lenses.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            ((IEnumerable)_lenses).GetEnumerator();
+    }
+
+    internal sealed class Lens : IEquatable<Lens>, IComparable<Lens>
     {
         private Lens(string label, int focalLength)
         {
             Label = label;
             FocalLength = focalLength;
-            Box = label.FancyHash();
+            BoxIndex = label.FancyHash();
         }
 
         internal int FocalLength { get; }
 
-        internal int Box { get; }
+        internal int BoxIndex { get; }
 
         internal string Label { get; }
 
-        internal static Lens? DecodeLens(string code) =>
-            code.Contains('=') ? DecodeLens(code.Split('=')) : null;
+        internal static Lens DecodeLens(string code) =>
+            code.Contains('=') ? DecodeLens(code.Split('=')) : new Lens(code.Split('-')[0], -1);
 
         private static Lens DecodeLens(IReadOnlyList<string> parts) =>
             new(parts[0], int.Parse(parts[1]));
+
+        public bool Equals(Lens? other) =>
+            Label.Equals(other?.Label);
+
+        public int CompareTo(Lens? other) =>
+            string.CompareOrdinal(Label, other?.Label);
     }
 }
 
