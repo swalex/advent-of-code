15a27a90538daa249cca95a48c14d9c9815e99b1
Alexander Saratow <a@saratow.biz>
2024-12-06 14:36:16 +0100

    Solve day 2

---
+++ AdventOfCode2024/Day02.fs
@@ -0,0 +1,37 @@
+﻿module Day02
+
+let private allDecreasing(input: int array): bool =
+    input
+    |> Array.pairwise
+    |> Array.forall (fun (a, b) -> a > b)
+
+let private allIncreasing(input: int array): bool =
+    input
+    |> Array.pairwise
+    |> Array.forall (fun (a, b) -> a < b)
+
+let private allWithinRange(input: int array): bool =
+    let isWithinRange(distance: int): bool =
+        distance >= 0 && distance <= 3
+    input
+    |> Array.pairwise
+    |> Array.forall (fun (a, b) -> abs(a - b) |> isWithinRange)
+
+let private isSafe(input: int array): bool =
+    (allDecreasing input || allIncreasing input) && allWithinRange input
+
+let private isDampenedSafe(input: int array): bool =
+    seq {
+        for i in 0..input.Length - 1 do
+            input |> Array.mapi (fun j x -> if j = i then None else Some x) |> Array.choose id
+    }
+    |> Seq.exists isSafe
+
+let private isSafeOrDampenedSafe(input: int array): bool =
+    isSafe input || isDampenedSafe input
+
+let solution1 (input: int array array): int =
+    input |> Array.filter isSafe |> Array.length
+
+let solution2 (input: int array array): int =
+    input |> Array.filter isSafeOrDampenedSafe |> Array.length
---
+++ AdventOfCode2024/DayXX.fs
@@ -0,0 +1,7 @@
+﻿module DayXX
+
+let solution1 (input: int array array): int =
+    0
+
+let solution2 (input: int array array): int =
+    0
