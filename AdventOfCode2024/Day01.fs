6fff6164bacb7046c2c9d4a43d5f1376d98a6864
Alexander Saratow <a@saratow.biz>
2024-12-06 10:51:56 +0100

    implement first solution for day 1

--- AdventOfCode2024/Day01.fs
+++ AdventOfCode2024/Day01.fs
@@ -1,7 +1,10 @@
 ﻿module Day01
 
-let solution1(input: string array array): int =
-    0
+let solution1(input: int array array): int =
+    let leftColumn = input |> Array.map (fun row -> row.[0]) |> Array.sort
+    let rightColumn = input |> Array.map (fun row -> row.[1]) |> Array.sort
+    let distances = Array.zip leftColumn rightColumn |> Array.map (fun (l, r) -> abs(r - l))
+    distances |> Array.sum
 
-let solution2(input: string array array): int =
+let solution2(input: int array array): int =
     0
