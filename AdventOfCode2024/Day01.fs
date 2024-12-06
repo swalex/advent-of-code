580a763a9cc21fc4fb5c609b342d57c9ec7657b6
Alexander Saratow <a@saratow.biz>
2024-12-06 11:01:51 +0100

    Optimize the first day 1 solution a bit

--- AdventOfCode2024/Day01.fs
+++ AdventOfCode2024/Day01.fs
@@ -1,10 +1,16 @@
 ﻿module Day01
 
 let solution1(input: int array array): int =
-    let leftColumn = input |> Array.map (fun row -> row.[0]) |> Array.sort
-    let rightColumn = input |> Array.map (fun row -> row.[1]) |> Array.sort
-    let distances = Array.zip leftColumn rightColumn |> Array.map (fun (l, r) -> abs(r - l))
-    distances |> Array.sum
+    let leftColumn, rightColumn =
+        input
+        |> Array.fold (fun (left, right) row -> (row.[0]::left, row.[1]::right)) ([], [])
+
+    let sortedLeft = leftColumn |> List.toArray |> Array.sort
+    let sortedRight = rightColumn |> List.toArray |> Array.sort
+
+    Array.zip sortedLeft sortedRight
+    |> Array.map (fun (l, r) -> abs(r - l))
+    |> Array.sum
 
 let solution2(input: int array array): int =
     0
