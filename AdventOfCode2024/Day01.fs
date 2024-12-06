77cc4b499f0183d3d969ec4a558ca645a3ba9dfd
Alexander Saratow <a@saratow.biz>
2024-12-06 11:08:19 +0100

    Implement second solution for day 1

--- AdventOfCode2024/Day01.fs
+++ AdventOfCode2024/Day01.fs
@@ -1,16 +1,29 @@
 ﻿module Day01
 
-let solution1(input: int array array): int =
-    let leftColumn, rightColumn =
-        input
-        |> Array.fold (fun (left, right) row -> (row.[0]::left, row.[1]::right)) ([], [])
+let splitToColumns (input: int array array): int array * int array =
+    input
+    |> Array.fold (fun (left, right) row -> (row.[0]::left, row.[1]::right)) ([], [])
+    |> fun (left, right) -> (left |> List.toArray, right |> List.toArray)
 
-    let sortedLeft = leftColumn |> List.toArray |> Array.sort
-    let sortedRight = rightColumn |> List.toArray |> Array.sort
+let solution1 (input: int array array): int =
+    let leftColumn, rightColumn = splitToColumns input
+
+    let sortedLeft = leftColumn |> Array.sort
+    let sortedRight = rightColumn |> Array.sort
 
     Array.zip sortedLeft sortedRight
     |> Array.map (fun (l, r) -> abs(r - l))
     |> Array.sum
 
-let solution2(input: int array array): int =
-    0
+let solution2 (input: int array array): int =
+    let leftColumn, rightColumn = splitToColumns input
+
+    let similarity (entry: int): int =
+        rightColumn
+        |> Array.filter (fun r -> r = entry)
+        |> Array.length
+        |> fun count -> count * entry
+
+    leftColumn
+    |> Array.map similarity
+    |> Array.sum
